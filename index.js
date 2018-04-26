f724f09a2de50fd9504db094daed7b50d39fa05d
Dan Budiac <dansays@gmail.com>
2018-04-26 07:04:02 -0400

    Add more jsdoc data

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 /**
+ * Thingy
  * @file Things Parser for Drafts 5
- * @author Daniel G. Budiac
- * 
- * Need a description of how to override symbols and extend
- * autotagger dictionary.
+ * @author Daniel G. Budiac <dansays@gmail.com>
+ * @see {@link https://github.com/dansays/thingy|Github}
 */
 
 
-/** Class representing a single things to-do item. */
+/** Class representing a single Things to-do item. */
 class Task {
-	
+
 	/**
-	 * Create a things to-do item.
-	 * @param {*} autotagger - A reference to an autotagger
+	 * Create a Things to-do item.
+	 * @param {Autotagger} autotagger - A reference to an autotagger
 	 */
 	constructor(autotagger) {
 		this._autotagger = autotagger;
@@ -20,18 +19,20 @@ class Task {
 		this._deadline = new ThingsDate();
 		this.attributes = { tags: [], 'checklist-items': [] };
 	}
-	
+
 	/**
 	 * The deadline to apply to the to-do. Relative dates are
 	 * parsed with the DateJS library.
+	 * @type {String}
 	 */
 	set deadline(date) {
 		this._deadline.date = date;
 		this.attributes.deadline = this._deadline.toString();
 	}
-	
+
 	/**
 	 * The title or ID of the project or area to add to.
+	 * @type {String}
 	 */
 	set list(nameOrId) {
 		let prop = this._isItemId(nameOrId) ? 'list-id' : 'list';
@@ -40,6 +41,7 @@ class Task {
 
 	/**
 	 * The text to use for the notes field of the to-do.
+	 * @type {String}
 	 */
 	set notes(notes) {
 		this.attributes.notes = notes.trim();
@@ -49,6 +51,7 @@ class Task {
 	 * The time to set for the task reminder. Overrides any time
 	 * specified in "when". Fuzzy times are parsed with the
 	 * DateJS library.
+	 * @type {String}
 	 */
 	set reminder(time) {
 		this._when.timeOverride = time.trim();
@@ -60,6 +63,7 @@ class Task {
 	 * autotagger, matching the string against a dictionary
 	 * of regular expressions and auto-applying attributes
 	 * for all matches.
+	 * @type {String}
 	 */
 	set title(value) {
 		this.attributes.title = value.trim();
@@ -76,18 +80,19 @@ class Task {
 		this.addTags(autotagged.tags || '');
 		this.addChecklistItems(autotagged.checklistItems || []);
 	}
-	
+
 	/**
 	 * The start date of the to-do. Values can be "today",
 	 * "tomorrow", "evening", "tonight", "anytime", "someday",
 	 * or a fuzzy date string with an optional time value,
 	 * which will add a reminder.
+	 * @type {String}
 	 */
 	set when(value) {
 		this._when.datetime = value.trim();
 		this.attributes.when = this._when.toString();
 	}
-	
+
 	/**
 	 * Add a checklist item to the to-do.
 	 * @param {String} item - The checklist item to add
@@ -98,7 +103,7 @@ class Task {
 
 	/**
 	 * Add an array of checklist items to the to-do
-	 * @param {Array} items - An array of checklist items to add
+	 * @param {String[]} items - An array of checklist items to add
 	 */
 	addChecklistItems(items = []) {
 		items = items
@@ -106,25 +111,26 @@ class Task {
 			.map(item => ({
 				type: 'checklist-item',
 				attributes: { title: item.trim() }
-			}));		
-		
+			}));
+
 		this.attributes['checklist-items'].push(...items);
 	}
 
 	/**
 	 * Add one or more tags to the to-do, separated by commas.
 	 * Tags that do not already exist will be ignored.
-	 * @param {*} tagCsvList 
+	 * @param {String} tagCsvList - A comma-separated list of one or more tags
 	 */
 	addTags(tagCsvList) {
 		let tagArr = tagCsvList.split(',').map(tag => tag.trim());
 		this.attributes.tags.push(...tagArr);
 	}
-	
+
 	/**
 	 * Export the current to-do, with all defined attributes,
 	 * as an object to be passed to the things:/// URL scheme.
-	 * https://support.culturedcode.com/customer/en/portal/articles/2803573#json
+	 * @see {@link https://support.culturedcode.com/customer/en/portal/articles/2803573#json|Things API documentation}
+	 * @return {Object} An object suitable to pass to the things:/// service
 	 */
 	toThingsObject() {
 		return {
@@ -136,12 +142,13 @@ class Task {
 	/**
 	 * Test whether a string is a things item ID
 	 * @param {String} value - The item name or id to test
+	 * @private
 	 */
 	_isItemId(value) {
 		const pattern = /^[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}$/img;
 		return pattern.test(value);
 	}
-	
+
 }
 
 
@@ -150,15 +157,24 @@ class Task {
 
 /** A class representing a date in Things */
 class ThingsDate {
-	
+
+	/** Create a new ThingsDate object */
+	constructor() {}
+
 	/**
 	 * The date value. In addition to fuzzy values parseable by DateJS,
 	 * valid values include "someday", "anytime", "evening", and "tonight".
+	 * @type {String}
 	 */
 	set date(value = '') {
 		this._datetime = value.trim().toLowerCase();
 	}
-	
+
+	/**
+	 * Convert to a Things-formatted string: YYYY-MM-DD@HH:MM, or
+	 * relative keyword: evening, someday, anytime.
+	 * @return {String} A Things-formatted date string
+	 */
 	toString() {
 		// This is still way too big. Some of this stuff
 		// should be parsed as values are set, and
@@ -201,7 +217,7 @@ class ThingsDate {
 		// the raw value and let Things take a crack at it.
 		let dt = Date.parse(datetime);
 		if (!dt) return datetime;
-		
+
 		// Override time if we explicitly set a reminder
 		if (timeOverride) {
 			let time = Date.parse(timeOverride);
@@ -221,89 +237,113 @@ class ThingsDate {
 		// Things will understand.
 		return this._formatThingsDate(dt, isEvening);
 	}
-	
+
 	/**
 	 * Test whether a string is shorthand for "tonight"
 	 * @param {String} value - The string to test
+	 * @return {boolean} True if string equals evening shorthand keywords
+	 * @private
 	 */
 	_isEveningShorthand(value) {
 		let pattern = /^((this )?evening|tonight)$/i;
 		return pattern.test(value);
 	}
-	
+
 	/**
 	 * Test whether a string is a dateless shorthand value
 	 * @param {String} value - The string to test
+	 * @return {boolean} True if string starts with "someday" or "anytime"
+	 * @private
 	 */
 	_isDateOnlyShorthand(value) {
 		let pattern = /^(someday|anytime)/i;
 		return pattern.test(value);
 	}
-	
+
 	/**
 	 * Test whether a time is ambiguously specified (lacking an am/pm
 	 * suffix) and possibly early in the morning.
 	 * @param {String} str - The raw, unparsed date
 	 * @param {DateJS} parsed - The datetime, parsed by DateJS
+	 * @return {boolean} True if AM/PM suffix is missing and hour is before 7
+	 * @private
 	 */
 	_isTimeEarlyAndAmbiguous(str, parsed) {
 		let hasAmPmSuffix = /\d *[ap]m?\b/i.test(str);
 		let isEarly = parsed.getHours() > 0 && parsed.getHours() < 7;
-		return hasAmPmSuffix && isEarly;
+		return !hasAmPmSuffix && isEarly;
 	}
-	
+
 	/**
 	 * Test whether a datetime is set to midnight
 	 * @param {DateJS} parsed - The datetime, parsed by DateJS
+	 * @return {boolean} True if time is midnight
+	 * @private
 	 */
 	_isTimeMidnight(parsed) {
 		let hours = parsed.getHours();
 		let minutes = parsed.getMinutes();
 		return hours + minutes == 0;
 	}
-	
+
 	/**
 	 * Format a DateJS datetime as a valid Things datetime string.
 	 * @param {DateJS} datetime - The datetime, parsed by DateJS
 	 * @param {boolean} forceEvening - Force to evening, overriding time value
+	 * @return {string} A Things-formatted date
+	 * @private
 	 */
 	_formatThingsDate(datetime, forceEvening) {
 		let date = datetime.toString('yyyy-MM-dd');
 		let time = datetime.toString('@HH:mm');
-		
+
 		if (this._isTimeMidnight(datetime)) time = '';
 		if (!this._allowTime) time = '';
-		
+
 		let isToday = datetime.between(Date.today(), Date.today().addDays(1));
 		let isEvening = forceEvening || datetime.getHours() > 17;
 		if (isToday && isEvening) date = 'evening';
 
 		return date + time;
 	}
-	
+
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
 
-/** A class representing a datetime in Things */
+/**
+ * A class representing a datetime in Things
+ * @extends ThingsDate
+ * */
 class ThingsDateTime
 extends ThingsDate {
-	
+
+	/** Create a new ThingsDate object */
 	constructor() {
 		super();
 		this._allowTime = true;
 	}
 
-	set datetime(value = '') {
+	/**
+	 * The date, with optional time. In addition to fuzzy values parseable by
+	 * DateJS, valid values include "someday", "anytime", "evening", and "tonight".
+	 * @type {String}
+	 */
+	set datetime(value) {
 		this._datetime = value.trim().toLowerCase();
 	}
-	
-	set timeOverride(value = '') {
+
+	/**
+	 * The time override. The datetime value's hour and minute
+	 * will be repolaced with the time override if specified.
+	 * @type {String}
+	 */
+	set timeOverride(value) {
 		this._timeOverride = value.trim().toLowerCase();
 	}
-	
+
 }
 
 
@@ -312,7 +352,7 @@ extends ThingsDate {
 
 /** A class representing a symbols dictionary */
 class Symbols {
-	
+
 	/**
 	 * Create a symbols dictionary
 	 * @param {Object} config - An optional object overriding one or
@@ -328,35 +368,38 @@ class Symbols {
 			notes:         '🗒',
 			checklistItem: '🔘'
 		};
-		
+
 		Object.assign(this._symbols, config);
 	}
 
 	/**
 	 * An array of all defined symbols.
+	 * @type {String[]}
 	 */
 	get all() {
 		return Object.values(this._symbols);
 	}
-	
+
 	/**
 	 * Look up a symbol based on an attribute name
 	 * @param {String} type - A valid Things to-do attribute name
+	 * @type {String}
 	 */
 	getSymbol(type) {
 		return this._symbols[type];
 	}
-	
+
 	/**
 	 * Look up an attribute name based on a symbol
 	 * @param {String} symbol - A symbol (emoji)
+	 * @type {String}
 	 */
 	getType(symbol) {
 		let symbols = this._symbols;
 		let keys = Object.keys(symbols);
 		return keys.filter(key => symbols[key] == symbol)[0];
 	}
-	
+
 }
 
 
@@ -365,7 +408,7 @@ class Symbols {
 
 /** A class representing a Things autotagger */
 class Autotagger {
-	
+
 	/**
 	 * Create a Things autotagger, including a small default dictionary
 	 * @param {Object} config - An array of objects to add to the dictionary
@@ -378,23 +421,23 @@ class Autotagger {
 			{ pattern: /^(Waiting For|WF) /i, tags: 'Waiting For' },
 			...config
 		];
-		
+
 		this._dictionary.forEach(entry => {
 			if (!entry.tags) return;
 			entry.tags = entry.tags.split(',');
 			entry.tags.map(entry => entry.trim());
 		});
 	}
-	
+
 	/**
 	 * Parse a string for matching autotagging entries
 	 * @param {Object} obj - An object containing task attributes
-	 * @returns {Object} An object containing updated task attributes
+	 * @return {Object} An object containing updated task attributes
 	 */
 	parse(title) {
 		const entries = [...this._dictionary]
 			.filter(item => item.pattern.test(title));
-				
+
 		let attributes = {};
 		entries.forEach(entry => {
 			Object.keys(entry).forEach(key => {
@@ -409,12 +452,13 @@ class Autotagger {
 
 		return attributes;
 	}
-	
+
 	/**
 	 * Update an object property. If the value is an array, push it real good.
 	 * @param {Object} obj - A reference to the source object
 	 * @param {String} prop - The name of the property to set
 	 * @param {Array|String} val - The value of the property to set
+	 * @private
 	 */
 	_setProp(obj, prop, val) {
 		if (Array.isArray(val)) {
@@ -424,7 +468,7 @@ class Autotagger {
 			obj[prop] = val;
 		}
 	}
-	
+
 }
 
 
@@ -433,7 +477,7 @@ class Autotagger {
 
 /** A class representing a Things task parser */
 class TasksParser {
-	
+
 	/**
 	 * Create a new Things task parser, generating a symbol
 	 * dictionary and autoparser.
@@ -446,23 +490,25 @@ class TasksParser {
 		this._symbols = new Symbols(symbolsConfig);
 		this._autotagger = new Autotagger(autotaggerConfig);
 	}
-	
+
 	/**
 	 * Parse a document containing Things tasks, and associated
 	 * attributes decorated by the appropriate Emoji symbols
+	 * @param doc {String} - The document to parse
+	 * @return {Object} An object suitable to pass to the things:/// service
 	 */
-	parse(value) {
+	parse(doc) {
 		// Break inline attribute references into their own lines
-		value = this._normalizeSymbols(value);
+		doc = this._normalizeSymbols(doc);
 
 		// Trim all leading/trailing whitespace from each line
-		value = this._trimWhitespace(value);
-		
+		doc = this._trimWhitespace(doc);
+
 		let tasks = []; // An array of task objects
 		let task; // The current task object
-		
-		value.split('\n').forEach(line => {
-			
+
+		doc.split('\n').forEach(line => {
+
 			let item = this._parseLine(line);
 
 			if (item && item.type == 'to-do') {
@@ -471,7 +517,7 @@ class TasksParser {
 				tasks.push(task);
 				return;
 			}
-						
+
 			// If we don't have an active task yet, or the
 			// current line couldn't be parsed, move along.
 			if (!task || !item) return;
@@ -496,30 +542,32 @@ class TasksParser {
 	 * @param {String} value - The document to parse
 	 * @return {String} A document with symbol-decorated
 	 *   attributes on their own line
+	 * @private
 	 */
 	_normalizeSymbols(value = '') {
 		let pattern = new RegExp(`(${this._symbols.all.join('|')})`, 'mg');
 		return value.replace(pattern, '\n$1');
 	}
-	
+
 	/**
 	 * Parse a line, mapping its symbol to a property
 	 * @param {String} line - The line to parse
-	 * @returns {Object} An object with the parsed property type and value
+	 * @return {Object} An object with the parsed property type and value
+	 * @private
 	 */
 	_parseLine(line) {
 		if (!line) return;
-		
+
 		// Lines with no symbol prefix are tasks.
 		if (/^[a-z0-9]/i.test(line)) {
 			return { type: 'to-do', value: line };
 		}
-		
+
 		let allSymbols = this._symbols.all;
 		let propPattern = new RegExp(`^(${allSymbols.join('|')})\s*(.*)$`, 'g');
 		let propMatch = propPattern.exec(line);
 		if (!propMatch || propMatch.length < 3) return;
-		
+
 		return {
 			type: this._symbols.getType(propMatch[1]),
 			value: propMatch[2]
@@ -530,13 +578,14 @@ class TasksParser {
 	 * Trim leading/trailing whitespace from each line of a document
 	 * @param {String} value - The value to trim
 	 * @return {String} A document with no leading or trailing whitespace
+	 * @private
 	 */
 	_trimWhitespace(value = '') {
 		return value
 			.replace(/^\s+(.+)/mg, '$1')
 			.replace(/(.+)\s+$/mg, '$1');
 	}
-	
+
 }
 
 
