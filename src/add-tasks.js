63def269a7ec7191df691977dc0729037eb72f86
Dan Budiac <dansays@gmail.com>
2018-04-26 20:12:33 -0400

    Add better task handling

--- src/add-tasks.js
+++ src/add-tasks.js
@@ -2,15 +2,45 @@ let symbolsConfig    = (typeof symbols    !== 'undefined') ? symbols    : {};
 let autotaggerConfig = (typeof autotagger !== 'undefined') ? autotagger : [];
 let parser = new TasksParser(symbolsConfig, autotaggerConfig);
 
-if (
-	typeof editor !== 'undefined'
-	&& typeof context !== 'undefined'
-	&& typeof CallbackURL !== 'undefined'
-) {
-	let text = editor.getSelectedText() || editor.getText();
-	let data = parser.parse(text);
+let document = getDocument();
+let data = parser.parse(document);
+let sent = sendToThings(data);
+
+if (sent === false) {
+	context.fail();
+} else if (sent === undefined) {
+	context.cancel('No tasks found');
+} else {
+	cleanup();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function getDocument() {
+	if (typeof editor === 'undefined') return '';
+	return editor.getSelectedText() || editor.getText();
+}
+
+function sendToThings(data) {
+	if (typeof CallbackURL === 'undefined') return false;
+	if (typeof context === 'undefined') return false;
+	if (data.length == 0) {
+		context.cancel('No tasks found');
+		return;
+	}
+
 	let callback = CallbackURL.create();
 	callback.baseURL = 'things:///json';
 	callback.addParameter('data', JSON.stringify(data));
-	callback.open() || context.fail();
+	return callback.open();
+}
+
+function cleanup() {
+	if (draft.isFlagged) return;
+	if (editor.getSelectedText()) return;
+	draft.isTrashed = true;
+	draft.addTag('Thingy');
+	draft.update();
+	Draft.create();
+	editor.activate();;
 }
